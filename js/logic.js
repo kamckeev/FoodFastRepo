e297a8ea0f9a5563dad52156b3607f154e8a0a80
unknown <annabl@med.unc.edu>
2019-06-09 21:57:31 -0400

    circles completed

--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,14 @@
 d3.csv("data/allFastFood.csv", function(fastFoodData){
-    // console.log(fastFoodData)
     var ncData=fastFoodData.filter(data=>data.province == "NC")
     console.log(ncData)
     var restaurants = ncData.map(place =>place.name)
     console.log(restaurants)
     cleanNames=[]
     restaurants.forEach(function(item){
-        item = item.replace("-"," ")
-        item = item.replace("'","")
+        item = item.replace("McDonald's","McDonalds")
+        item = item.replace("Quizno's","Quiznos")
         item = item.replace(' of Rolesville','')
+        item = item.replace('SONIC Drive In','SONIC Drive-In')
         
         cleanNames.push(item)
     })
@@ -20,12 +20,66 @@ d3.csv("data/allFastFood.csv", function(fastFoodData){
             return name == item
         }
         var num=cleanNames.filter(checkName).length
-        var logolink=`<img src="//logo.clearbit.com/${item}.com">`
+        var logolink=''
+       
+        var cleanName =''
+        
+        cleanName = item.replace (/'/g,"")
+        cleanName = cleanName.replace (/ /g,"")
+
+        if (item == "Burger King") {
+          logolink = "//logo.clearbit.com/bk.com"
+        }
+        else if (item == "Bojangles' Famous Chicken 'n Biscuits") {
+          logolink = "//logo.clearbit.com/bojangles.com"
+        }
+        else if (item == "Papa John's Pizza") {
+          logolink = "//logo.clearbit.com/papajohns.com"
+        }
+        else if (num >=5){
+          logolink = `//logo.clearbit.com/${cleanName}.com`
+        }
+        else {
+          logolink =''
+        }
+
+      var color=''
+      // if (item == "Burger King"|| item == "Arby's"|| item == "Hardee's"||item == "Domino's Pizza"|| item == "Pizza Hut"|| item =="Wendy's"){
+      //   color = "white"
+      // }
+      if (item == "McDonalds") {
+        color = "#bd0016"
+      }
+      else if (item == "Subway") {
+        color = "#028840"
+      }
+      else if (item == "Chick-Fil-A") {
+        color = "#b00027"
+      }
+      else if (item == "Bojangles' Famous Chicken 'n Biscuits") {
+        color = "#fdb646"
+      }
+      else if (item == "Panera Bread") {
+        color = "#687718"
+      }
+      else if (item == "Papa John's Pizza") {
+        color = "#bc121d"
+      }
+      else if (item == "Waffle House") {
+        color = "black"
+      }
+      
+      else {color='white'}
+
+
+
+
         totals.push({
             r:num*3,
             name: item,
             count: num,
-            logo: logolink          
+            logo: logolink,
+            color: color          
         })       
     })
     console.log(totals)
@@ -35,8 +89,8 @@ d3.csv("data/allFastFood.csv", function(fastFoodData){
     console.log(data)
 var packed = d3.packSiblings(data.children);
 
-var w = 1200
-var h = 1000
+var w = 900
+var h = 700
 var svg = d3.select("body")
   .append("svg")
   .attr("width", w)
@@ -59,20 +113,52 @@ var nodes = svg.selectAll(null)
   .attr("r", function(d) {
     return d.r
   })
-  .style("fill", 'blue')
-  .style('opacity',.5)
+  .style("fill", d=>d.color)
+  .style('stroke','black')
+  // .style('stroke-width',2)
+  .style('opacity',1)
   
-  
-  
+
+  svg.selectAll(null)
+  .data(data.children)
+  .enter()
+  .append('image')
+  .attr('xlink:href',d=>d.logo)
+  .attr('x',d=>d.x-(d.r/2))  
+  .attr('y',d=>d.y-(d.r/2))
+  .attr('width', d=>d.r)
+  .attr('height',d=>d.r)
+
 var toolTip = d3.select("body").append('div')
     .attr('class','tooltip')
 
 nodes.on('mouseover',function(d, i) {
     toolTip.style("display", "block");
-    toolTip.html(`${d.name} <br> ${d.count}<br> ${d.logo}`)
+    toolTip.html(`${d.name} <br> ${d.count}`)
       .style("left", d3.event.pageX + "px")
       .style("top", d3.event.pageY + "px")
+    
     })
     .on("mouseout", function() {
         toolTip.style("display", "none")})
+
+
+
+
+
+  // anime({
+  //   targets: 'circle',
+  //   translateX: 1000,
+  //   easing: 'easeInOutExpo',
+  //   delay: anime.stagger(50)
+  // });
+  // anime({
+  //   targets: 'image',
+  //   translateX: 1000,
+  //   easing: 'easeInOutExpo',
+  //   delay: anime.stagger(50)
+  // });
+  
+
+
 })

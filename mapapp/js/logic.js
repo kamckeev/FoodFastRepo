f458f09066cd921452a92b09f53f7e17daecb2ee
Kim.McK <k.mckeever@alumni.unc.edu>
2019-06-11 20:56:07 -0400

    major update

--- mapapp/js/logic.js
+++ mapapp/js/logic.js
@@ -1,22 +1,25 @@
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+var streetmap = L.tileLayer(
+//  "https://api.mapbox.com/styles/v1/saucyocelot/cjwnrc2ni2lvt1cnyney377u0?access_token={accessToken}", {
+ "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}",{
+  
+      attribution: "Map data &copy; <a href=\"https://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
       maxZoom: 18,
       id: "mapbox.streets",
       accessToken: API_KEY
     });
     
-    var baseMaps = {
-      "Light Map": streetmap
-    };
-    
- 
-    var myMap=L.map("map-id", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 4,
-      layers:[streetmap]
-    })
+var baseMaps = {
+  "Light Map": streetmap
+};
+
+
+var myMap=L.map("map-id", {
+  center: [
+    35.782169, -80.793457
+  ],
+  zoom: 7,
+  layers:[streetmap]
+});
 
 // Adding tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -26,69 +29,82 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Link to GeoJSON
-var APILink = "http://data.beta.nyc//dataset/d6ffa9a4-c598-4b18-8caf-14abde6a5755/resource/74cdcc33-512f-439c-" +
-"a43e-c09588c4b391/download/60dbe69bcd3640d5bedde86d69ba7666geojsonmedianhouseholdincomecensustract.geojson";
+// // Link to GeoJSON
+// var APILink = "http://data.beta.nyc//dataset/d6ffa9a4-c598-4b18-8caf-14abde6a5755/resource/74cdcc33-512f-439c-" +
+// "a43e-c09588c4b391/download/60dbe69bcd3640d5bedde86d69ba7666geojsonmedianhouseholdincomecensustract.geojson";
 
-var geojson;
+// var geojson = ff_geodataset;
+// console.log(geojson)
 
-// Grab data with d3
-d3.json(APILink, function(data) {
 
-  // Create a new choropleth layer
-  geojson = L.choropleth(data, {
+// // Grab data with d3
+d3.json("ff_geodataset.json", function(data) {
+    console.log(data)
 
-    // Define what  property in the features to use
-    valueProperty: "MHI",
+    markerLayer = L.geoJSON(data,
+      {
+        pointToLayer: function(geoJsonPoint, latlng) {
+          return L.marker(latlng, {
+            icon: function(p){
+              return L.icon({
+                iconSize: [100, 195],
+                iconAnchor: [22, 94],
+                popupAnchor: [-3, -76],
+                // shadowUrl: 'my-icon-shadow.png',
+                // shadowSize: [68, 95],
+                // shadowAnchor: [22, 94]
+            }) 
+          }
+        })
+      }}).addTo(myMap)
 
-    // Set color scale
-    scale: ["#ffffb2", "#b10026"],
+})
+//   // Create a new choropleth layer
+  //geojson = L.choropleth(data, {
 
-    // Number of breaks in step range
-    steps: 10,
+//     // Define what  property in the features to use
+    //valueProperty: "MHI",
 
-    // q for quartile, e for equidistant, k for k-means
-    mode: "q",
-    style: {
-      // Border color
-      color: "#fff",
-      weight: 1,
-      fillOpacity: 0.8
-    },
+//     // Set color scale
+//     scale: ["#ffffb2", "#b10026"],
 
-    // // Binding a pop-up to each layer
-    // onEachFeature: function(feature, layer) {
-    //   layer.bindPopup(feature.properties.LOCALNAME + ", " + feature.properties.State + "<br>Median Household Income:<br>" +
-    //     "$" + feature.properties.MHI);
-    // }
-  }).addTo(myMap);
+//     // Number of breaks in step range
+//     steps: 10,
 
-  // Set up the legend
-  var legend = L.control({ position: "bottomright" });
-  legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "info legend");
-    var limits = geojson.options.limits;
-    var colors = geojson.options.colors;
-    var labels = [];
+//     // q for quartile, e for equidistant, k for k-means
+//     mode: "q",
+//     style: {
+//       // Border color
+//       color: "#fff",
+//       weight: 1,
+//       fillOpacity: 0.8
+//     },
 
-    // Add min & max
-    var legendInfo = "<h1>Median Income</h1>" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
 
-    div.innerHTML = legendInfo;
+//   }).addTo(myMap);
 
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
 
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-  };
+   
 
-  // Adding legend to the map
-  legend.addTo(myMap);
+//   // Set up the legend
+//   var legend = L.control({ position: "bottomright" });
+//   legend.onAdd = function() {
+//     var div = L.DomUtil.create("div", "info legend");
+//     var limits = geojson.options.limits;
+//     var colors = geojson.options.colors;
+//     var labels = [];
 
-});
+//     div.innerHTML = legendInfo;
+
+//     limits.forEach(function(limit, index) {
+//       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+//     });
+
+//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+//     return div;
+//   };
+
+//   // Adding legend to the map
+//   legend.addTo(myMap);
+
+// });
